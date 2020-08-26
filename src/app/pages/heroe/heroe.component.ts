6964b6e49aa473a6fcc757871c3283903fe11e45
Luis Mayta <luis.mayta@gmail.com>
2020-08-26 18:20:33 -0500

    creacion de registros

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroeModel } from 'src/app/models/heroe.model';
+import { NgForm } from '@angular/forms';
+import { HeroesService } from 'src/app/services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  heroe: HeroeModel = new HeroeModel();
+
+  constructor(private heroesServices:HeroesService) { }
 
   ngOnInit(): void {
+  }
+  guardar(form: NgForm){
+    if (form.invalid) {
+      console.log('Formulario invalido');
+      return;
+    }
+    this.heroesServices.crearHeroe(this.heroe)
+                       .subscribe(resp=>{
+                         console.log(resp);
+                         this.heroe = resp;
+                       })
+
   }
 
 }
